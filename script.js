6fb0126b2b96654bc3a0ef1a150cf7a98f49234b
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-13 16:45:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", (event) => {
-    event.preventDefault(); // prevent page reload
+    event.preventDefault();
 
     let name = document.getElementById("name").value.trim();
     let age = document.getElementById("age").value.trim();
 
     if (name.length === 0 || age === '') {
-        alert("Please enter valid details.");
-        return; // stop execution
+        alert("Please enter valid details");
+        return;
     }
 
-    let myPromise = () => {
-        return new Promise((res, rej) => {
-            setTimeout(() => {
-                if (parseInt(age) >= 18) res();
-                else rej();
-            }, 4000);
-        });
-    };
-
-    myPromise()
-        .then(() => {
-            alert(`Welcome, ${name}. You can vote.`);
-        })
-        .catch(() => {
-            alert(`Oh sorry ${name}. You aren't old enough.`);
-        });
+    new Promise((res, rej) => {
+        setTimeout(() => {
+            if (parseInt(age) >= 18) res();
+            else rej();
+        }, 4000);
+    })
+    .then(() => {
+        alert(`Welcome, ${name}. You can vote.`);
+    })
+    .catch(() => {
+        alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
 });
