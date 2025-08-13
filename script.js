e521ecddc88f07a244591a7de6d1904ba2d54f3a
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-13 16:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", (event) => {
+    event.preventDefault(); // prevent page reload
+
+    let name = document.getElementById("name").value.trim();
+    let age = document.getElementById("age").value.trim();
+
+    if (name.length === 0 || age === '') {
+        alert("Please enter valid details.");
+        return; // stop execution
+    }
+
+    let myPromise = () => {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                if (parseInt(age) >= 18) res();
+                else rej();
+            }, 4000);
+        });
+    };
+
+    myPromise()
+        .then(() => {
+            alert(`Welcome, ${name}. You can vote.`);
+        })
+        .catch(() => {
+            alert(`Oh sorry ${name}. You aren't old enough.`);
+        });
+});
